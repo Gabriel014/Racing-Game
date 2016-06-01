3d7407ab571bbfccb5f59897221f1c46a8a0d3b5
Gabriel Alonso <formmo@live.com>
2016-06-01 19:23:28 -0400

    Obrigado Jesus
    Funcionou
    Amém

--- Racing Game/Assets/Scripts/Game/Generator.cs
+++ Racing Game/Assets/Scripts/Game/Generator.cs
@@ -2,68 +2,77 @@
 using System.Collections;
 
 public class Generator : MonoBehaviour {
-
-    public int nextRoad, nextCar, nextObstacle;
-
-    public GameObject road1, road2, road3;
-    public GameObject car1, car2;
-    public GameObject fuelObject;
-    public GameObject obstacle1, obstacle2;
-    public GameObject finishLine;
-
-    public float obstacleRate, fuelRate, carRate, roadRate, finishLineTimer;
-
+	
+	public int nextRoad, nextCar, nextObstacle;
+	
+	public GameObject road1, road2, road3;
+	public GameObject car1, car2;
+	public GameObject fuelObject;
+	public GameObject obstacle1, obstacle2;
+	public GameObject finishLine;
+	
+	public float obstacleRate, fuelRate, carRate, roadRate, finishLineTimer;
+	
 	// Use this for initialization
 	void Start () {
-
-        nextCar = Random.Range(1, 5);
-        nextRoad = Random.Range(1, 6);
-        nextObstacle = Random.Range(1, 3);
-
-        InvokeRepeating("GenerateFuel", fuelRate, fuelRate);
-        InvokeRepeating("GenerateObstacle", obstacleRate, obstacleRate);
-        InvokeRepeating("GenerateCar", carRate, carRate);
-        InvokeRepeating("GenerateRoad", roadRate, roadRate);
-        Invoke("FinishLineCreator", finishLineTimer);
-
+		
+		nextCar = Random.Range(1, 5);
+		nextRoad = Random.Range(1, 6);
+		nextObstacle = Random.Range(1, 3);
+		
+		InvokeRepeating("GenerateFuel", fuelRate, fuelRate);
+		InvokeRepeating("GenerateObstacle", obstacleRate, obstacleRate);
+		InvokeRepeating("GenerateCar", carRate, carRate);
+		InvokeRepeating("GenerateRoad", roadRate, roadRate);
+		Invoke("FinishLineCreator", finishLineTimer);
+		
 	}
+	
+	
+	void GenerateFuel() {
+		Instantiate(fuelObject);
+	}
+	
+	void GenerateCar() {
+		// Generate a random kind of car on the road each carRate seconds, this can be changed so more cars can be added
+		if (nextCar <= 3) Instantiate(car1);
+		if (nextCar > 3) Instantiate(car2);
+		
+		nextCar = Random.Range(1, 5);
+		
+	}
+	
+	void GenerateObstacle() {
+		// Generate a random kind of obstacle each obstacleRate seconds, this can be changed so more obstacles can be added
+		if (nextObstacle == 1) Instantiate(obstacle1);
+		if (nextObstacle == 2) Instantiate(obstacle2);
+		
+		nextObstacle = Random.Range(1, 3);
+	}
+	
+	void GenerateRoad() {
+		// Generates a new kind of road each roadRate seconds, this one has to be synchronized with the road speed so
+		// it will create a parallax effect when the game is running (kinda like in Gil's game's parallax). 
+		if (nextRoad == 1) {
+			Instantiate (road2);
+			nextRoad = 3;
+		}
 
+		else if (nextRoad == 2){
+			Instantiate (road3);
+			nextRoad = 3;
+		}
 
-    void GenerateFuel() {
-        Instantiate(fuelObject);
-}
-
-    void GenerateCar() {
-        // Generate a random kind of car on the road each carRate seconds, this can be changed so more cars can be added
-        if (nextCar <= 3) Instantiate(car1);
-        if (nextCar > 3) Instantiate(car2);
-
-        nextCar = Random.Range(1, 5);
-
-    }
-
-    void GenerateObstacle() {
-        // Generate a random kind of obstacle each obstacleRate seconds, this can be changed so more obstacles can be added
-        if (nextObstacle == 1) Instantiate(obstacle1);
-        if (nextObstacle == 2) Instantiate(obstacle2);
-
-        nextObstacle = Random.Range(1, 3);
-    }
-
-    void GenerateRoad() {
-        // Generates a new kind of road each roadRate seconds, this one has to be synchronized with the road speed so
-        // it will create a parallax effect when the game is running (kinda like in Gil's game's parallax). 
-        if (nextRoad == 1) Instantiate(road2);
-        else if (nextRoad == 2) Instantiate(road3);
-        else Instantiate(road1);
-
-        nextRoad = Random.Range(1, 6);
-
-    }
-
-    void FinishLineCreator(){
-        // Creates the finish line after finishLineTimer seconds has elapsed, this one can be changed according to the
-        // current stage.
-        Instantiate(finishLine);
-    }
+		else {
+			Instantiate (road1);
+			nextRoad = Random.Range (1, 6);
+		}
+		
+	}
+	
+	void FinishLineCreator(){
+		// Creates the finish line after finishLineTimer seconds has elapsed, this one can be changed according to the
+		// current stage.
+		Instantiate(finishLine);
+	}
 }
