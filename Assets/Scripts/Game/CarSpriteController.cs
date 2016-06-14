141926a5e22e65ddd39af4cc606524a134e9e570
BrazilGabriel <gabriel.br1994@gmail.com>
2016-06-14 11:52:32 -0400

    adição da seleção de carro
    agora o player pode alterar o carro, a

---
+++ Assets/Scripts/Game/CarSpriteController.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class CarSpriteController : MonoBehaviour {
+
+	public GameObject Car;
+	public GameObject EnemyCar1;
+	public GameObject EnemyCar2;
+
+	public Sprite model1, model2, model3, model4, model5;
+	public int carModel;
+
+	// Use this for initialization
+	void Start () {
+
+		string playerChoice = PlayerPrefs.GetString("car");
+
+		if (playerChoice == "1"){
+			Car.GetComponent<SpriteRenderer>().sprite = model1;
+			EnemyCar1.GetComponent<SpriteRenderer>().sprite = model2;
+			EnemyCar2.GetComponent<SpriteRenderer>().sprite = model3;
+		}
+		if (playerChoice == "2"){
+			Car.GetComponent<SpriteRenderer>().sprite = model2;
+			EnemyCar1.GetComponent<SpriteRenderer>().sprite = model3;
+			EnemyCar2.GetComponent<SpriteRenderer>().sprite = model4;
+		}
+		if (playerChoice == "3"){
+			Car.GetComponent<SpriteRenderer>().sprite = model3;
+			EnemyCar1.GetComponent<SpriteRenderer>().sprite = model4;
+			EnemyCar2.GetComponent<SpriteRenderer>().sprite = model5;
+		}
+		if (playerChoice == "4"){
+			Car.GetComponent<SpriteRenderer>().sprite = model4;
+			EnemyCar1.GetComponent<SpriteRenderer>().sprite = model5;
+			EnemyCar2.GetComponent<SpriteRenderer>().sprite = model1;
+		}
+		if (playerChoice == "5"){
+			Car.GetComponent<SpriteRenderer>().sprite = model5;
+			EnemyCar1.GetComponent<SpriteRenderer>().sprite = model1;
+			EnemyCar2.GetComponent<SpriteRenderer>().sprite = model2;
+		}
+	}
+}
---
+++ Assets/Scripts/UI/CarSelect.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CarSelect : MonoBehaviour {
+
+	// Use this for initialization
+	public void Choose1 () {
+
+		PlayerPrefs.SetString("car","1");
+	}
+
+	public void Choose2 () {
+
+		PlayerPrefs.SetString("car","2");
+	}
+
+	public void Choose3 () {
+
+		PlayerPrefs.SetString("car","3");
+	}
+
+	public void Choose4 () {
+
+		PlayerPrefs.SetString("car","4");
+	}
+
+	public void Choose5 () {
+
+		PlayerPrefs.SetString("car","5");
+	}		
+}
