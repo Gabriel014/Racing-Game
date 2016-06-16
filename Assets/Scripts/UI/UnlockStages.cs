0b16480159f67a1cb911e0f6883471fae8ff03c5
BrazilGabriel <gabriel.br1994@gmail.com>
2016-06-16 18:41:12 -0400

    Revert "Revert "Correção nos botões + hack""
    This reverts commit dac4d0fb8e37578e77b0710a2d749b9c540e087f.

---
+++ Assets/Scripts/UI/UnlockStages.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UnlockStages : MonoBehaviour {
+	
+	// Update is called once per frame
+	public void UnlockItAll () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            PlayerPrefs.SetString("clear1", "true");
+            PlayerPrefs.SetString("clear2", "true");
+            PlayerPrefs.SetString("clear3", "true");
+            PlayerPrefs.SetString("clear4", "true");
+        }
+	}
+}
